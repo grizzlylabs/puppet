ae2fe859f72a9a2c2c1a9fdfa337bdfd57a80ede
grizzly <grizzly@localhost.localdomain>
2015-06-23 09:14:07 -0400

    first commit

---
+++ scripts/install_puppet_agent-centos7.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Installs puppet on CentOS 7
+# Uses defaults
+
+PUPPET_GID='2000'
+
+sudo groupadd -g "$PUPPET_GID" puppet
+sudo usermod -a -G puppet "$USER"
+
+sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+sudo yum install puppet -y
+
+sudo mkdir -p /etc/puppet/modules/puppet/mainfests
+sudo mkdir -p /etc/puppet/manifests
+
+sudo chown -R root:puppet /etc/puppet 
+sudo chmod -R 770         /etc/puppet 
+sudo chmod -R g+s         /etc/puppet 
+
+
