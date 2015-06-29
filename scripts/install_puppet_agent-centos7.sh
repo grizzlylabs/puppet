448c5971ae89700b908e955684f3e87912d07d65
grizzly <grizzly@bootstrap.grizzlylabs.org>
2015-06-29 08:33:11 -0400

    rename

--- scripts/install_puppet_agent-centos7.sh
+++ scripts/install_puppet_agent-centos7.sh
@@ -10,7 +10,7 @@ sudo usermod -a -G puppet "$USER"
 sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 sudo yum install puppet -y
 
-sudo mkdir -p /etc/puppet/modules/puppet/manifests
+sudo mkdir -p /etc/puppet/modules/puppet_service/manifests
 sudo mkdir -p /etc/puppet/manifests
 
 sudo chown -R root:puppet /etc/puppet 
